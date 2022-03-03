a497c0a1c4eccd05c44804733d52e77e3f10a1d2
Raymond Chen <ahrchen@gmail.com>
2022-03-03 15:47:15 -0800

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Raymond Chen on 3/3/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
